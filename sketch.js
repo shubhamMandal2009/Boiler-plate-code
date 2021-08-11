88da7b4a598fbe2a20d2785efb40d4756e8f7aec
shubhamMandal2009 <88720888+shubhamMandal2009@users.noreply.github.com>
2021-08-11 17:44:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+/*
+
+
+write your own story
+
+*/
+
+function preload(){
+  
+  
+
+}
+
+
+
+function setup() {
+  createCanvas(600, 400);
+  
+ 
+}
+
+function draw() {
+  background("lightblue");
+  
+ 
+  
+  drawSprites();
+
+}
+
